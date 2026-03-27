917125f13f83f18dc3214efd8a8e69c32556c2bd
shubham-88 <shubham.mca@gmail.com>
2026-03-28 00:39:46 +0530

    Data Model creation Itr1.

--- pv-app/src/main/resources/db/changelog/migrations/001-init.sql
+++ pv-app/src/main/resources/db/changelog/migrations/001-init.sql
@@ -1,37 +1,50 @@
-CREATE TABLE PLANT (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+--Stores plants/units - kashipur, roorkee, haridwar
+CREATE TABLE business_units
+(
+    id         BIGSERIAL PRIMARY KEY,
+    code       VARCHAR(20)  NOT NULL UNIQUE,
+    name       VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
--- 1. Users table
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    enabled BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE UNIQUE INDEX idx_business_units_code ON business_units (code);
+CREATE UNIQUE INDEX idx_business_units_name ON business_units (name);
+
+--Stores System Users - employee working in the business unit
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    code       VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    enabled    BOOLEAN   DEFAULT TRUE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX idx_users_code ON users (code);
+CREATE UNIQUE INDEX idx_users_email ON users (email);
+
+--Stores users roles - ADMIN, CFT
+CREATE TABLE roles
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- 2. Roles table (e.g., ROLE_ADMIN, ROLE_USER)
-CREATE TABLE roles (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+--Sores privileges given to a user - PERFORM_STORE_VISIT, SUBMIT_DATA,EXPORT
+CREATE TABLE privileges
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- 3. Privileges table (e.g., READ_PRIVILEGE, WRITE_PRIVILEGE)
-CREATE TABLE privileges (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- 4. Map Users to Roles
-CREATE TABLE users_roles (
+--Stores Users to Roles mapping
+CREATE TABLE users_roles
+(
     user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
     PRIMARY KEY (user_id, role_id),
@@ -39,19 +52,126 @@ CREATE TABLE users_roles (
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
 );
 
--- 5. Map Roles to Privileges
-CREATE TABLE roles_privileges (
-    role_id BIGINT NOT NULL,
+--Stores Roles to Privileges mapping
+CREATE TABLE roles_privileges
+(
+    role_id      BIGINT NOT NULL,
     privilege_id BIGINT NOT NULL,
     PRIMARY KEY (role_id, privilege_id),
     CONSTRAINT fk_role_rel FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
     CONSTRAINT fk_privilege_rel FOREIGN KEY (privilege_id) REFERENCES privileges (id) ON DELETE CASCADE
 );
 
------------------------Masters
+--Store form templetes - used to store diffrent locations form staructure
+CREATE TABLE form_template
+(
+    id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name       VARCHAR(255) NOT NULL,
+    loc_id     BIGINT       NOT NULL,
+    attributes JSONB        NOT NULL,
+    created_at TIMESTAMPTZ        DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ        DEFAULT CURRENT_TIMESTAMP,
 
-CREATE TABLE inspection_status (
-    id SERIAL PRIMARY KEY,
-    code VARCHAR(20) UNIQUE NOT NULL, -- e.g., 'PENDING'
-    display_name VARCHAR(50) NOT NULL -- e.g., 'Pending Approval'
+    CONSTRAINT uq_form_template_name_loc UNIQUE (name, loc_id),
+    CONSTRAINT fk_form_template_location
+        FOREIGN KEY (loc_id)
+            REFERENCES locations (id)
+
 );
+
+-- Indexing the JSONB column for faster lookups
+CREATE INDEX idx_form_template_attr ON form_template USING GIN (attributes);
+CREATE INDEX idx_form_template_name ON form_template (name);
+CREATE INDEX idx_form_template_loc_id ON form_template (loc_id);
+
+-- Stores Location types - BASE_LOC-1, SUB_LOCATION-2 , SUB_SUB_LOCATION-3
+CREATE TABLE location_type
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    code VARCHAR(20)  NOT NULL UNIQUE
+);
+
+CREATE INDEX idx_location_type_code ON location_type (code);
+CREATE INDEX idx_location_type_name ON location_type (name);
+
+--Stores Locations - Production , SCM , Blade Packing
+CREATE TABLE location
+(
+    id               BIGSERIAL PRIMARY KEY,
+    name             VARCHAR(255) NOT NULL,
+    location_type_id BIGINT       NOT NULL,
+    parent_loc_id    BIGINT,
+    unit_id          BIGINT,
+    created_at       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_location_type
+        FOREIGN KEY (location_type_id) REFERENCES location_type (id),
+
+    CONSTRAINT fk_parent_location
+        FOREIGN KEY (parent_loc_id) REFERENCES location (id),
+
+    CONSTRAINT fk_location_unit
+        FOREIGN KEY (unit_id) REFERENCES business_units (id)
+);
+
+
+CREATE INDEX idx_location_parent ON location (parent_loc_id);
+CREATE INDEX idx_location_name ON location (name);
+CREATE INDEX idx_location_unit_id ON location (unit_id);
+
+--Stores form fields types - Dropdown. radio button, input text, text area
+CREATE TABLE form_field_types
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL UNIQUE,
+    short_name  VARCHAR(10),
+    description VARCHAR(255)
+);
+
+--Stores form fields data types - integer, long, flot , double, string
+CREATE TABLE form_field_data_types
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL UNIQUE,
+    short_name  VARCHAR(10),
+    description VARCHAR(255)
+);
+
+--Stores all available master data in the system - Basket type, Spool , Bobbin
+CREATE TABLE master_data_registry
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL UNIQUE,
+    table_name VARCHAR(63)  NOT NULL
+    -- 63 is the max length for identifiers in Postgres
+);
+
+
+CREATE INDEX idx_master_code ON master_data_registry (name);
+CREATE INDEX idx_data_type_short ON form_field_data_types (name);
+CREATE INDEX idx_type_short ON form_field_types (name);
+
+--Store inspection status , PENDING / APPROVED
+CREATE TABLE inspection_status
+(
+    id           SERIAL PRIMARY KEY,
+    code         VARCHAR(20) UNIQUE NOT NULL, -- e.g., 'PENDING'
+    display_name VARCHAR(50)        NOT NULL  -- e.g., 'Pending Approval'
+);
+
+--Stores basket master data - 1,2,3
+CREATE TABLE basket
+(
+    id            BIGSERIAL PRIMARY KEY,
+    name          VARCHAR(255)   NOT NULL UNIQUE,
+    code          VARCHAR(50)    NOT NULL UNIQUE,
+    basket_number INTEGER        NOT NULL UNIQUE,
+    self_weight   NUMERIC(10, 3) NOT NULL, -- Precision for weight
+    created_at    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Index for code as it will likely be used in search/barcode lookups
+CREATE INDEX idx_basket_code ON basket (code);
