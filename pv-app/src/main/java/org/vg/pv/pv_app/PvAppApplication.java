a83e573a67b3e9f3ee4b9bd8f38666e90cd31fbd
shubham-88 <shubham.mca@gmail.com>
2026-04-12 23:29:07 +0530

    db and podman compose setup

--- pv-app/src/main/java/org/vg/pv/pv_app/PvAppApplication.java
+++ pv-app/src/main/java/org/vg/pv/pv_app/PvAppApplication.java
@@ -1,14 +1,20 @@
 package org.vg.pv.pv_app;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PvAppApplication {
+
 
 	public static void main(String[] args) {
         System.out.println("JVM TimeZone: " + java.util.TimeZone.getDefault().getID());
 		SpringApplication.run(PvAppApplication.class, args);
 	}
+
+
 
 }
